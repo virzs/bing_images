c10f1a6ffb002315e950eed185687ceeb10caf2c
vir <virs98@outlook.com>
2024-11-19 13:55:12 +0800

    feat(service): :sparkles: 支持处理之前的数据是否为当前语言

--- service/src/routes/bing_image.rs
+++ service/src/routes/bing_image.rs
@@ -1,13 +1,13 @@
 use crate::models::bing_image::BingImage;
-use mongodb::bson::{doc, Document};
-use rocket::State;
-use mongodb::{Collection};
 use futures::stream::TryStreamExt;
-use rocket::Route;
+use mongodb::bson::{doc, Document};
+use mongodb::Collection;
+use rocket::form::FromForm;
 use rocket::serde::json::Json;
 use rocket::serde::Deserialize;
-use rocket::form::FromForm;
 use rocket::yansi::Paint;
+use rocket::Route;
+use rocket::State;
 use rocket_okapi::{openapi, openapi_get_routes};
 use schemars::JsonSchema;
 
@@ -19,6 +19,16 @@ pub struct FilterParams {
     page_size: Option<i64>,
 }
 
+fn create_bing_image(img: BingImage, base_url: &str, market: &str) -> BingImage {
+    BingImage {
+        url: format!("{}{}", base_url, img.url.as_deref().unwrap_or_default()).into(),
+        urlbase: format!("{}{}", base_url, img.urlbase.as_deref().unwrap_or_default()).into(),
+        quiz: format!("{}{}", base_url, img.quiz.as_deref().unwrap_or_default()).into(),
+        mkt: Some(market.to_string()),
+        ..img
+    }
+}
+
 async fn fetch_and_store_image(
     client: &reqwest::Client,
     url: &str,
@@ -27,7 +37,8 @@ async fn fetch_and_store_image(
     base_url: &str,
     collection: &State<Collection<BingImage>>,
 ) {
-    let res = client.get(url)
+    let res = client
+        .get(url)
         .query(query_params)
         .send()
         .await
@@ -38,23 +49,41 @@ async fn fetch_and_store_image(
 
     info!("调用预定任务Bing图像API成功. 区域 {}", market);
 
-    if let Some(img) = res["images"].as_array().and_then(|arr| arr.get(0)) {
-        let img: BingImage = serde_json::from_value(img.clone()).unwrap();
+    if let Some(images) = res["images"].as_array() {
+        for img_value in images {
+            let img: BingImage = serde_json::from_value(img_value.clone()).unwrap();
 
-        info!("图像 {}", img.title.clone().unwrap_or("无标题".to_string()));
+            info!("图像 {}", img.title.clone().unwrap_or("无标题".to_string()));
 
-        let has = collection.find_one(doc! { "startdate": &img.startdate, "mkt": market }).await.unwrap();
-        if has.is_none() {
-            collection.insert_one(BingImage {
-                url: format!("{}{}", base_url, img.url.unwrap_or_default()).into(),
-                urlbase: format!("{}{}", base_url, img.urlbase.unwrap_or_default()).into(),
-                quiz: format!("{}{}", base_url, img.quiz.unwrap_or_default()).into(),
-                mkt: Some(market.to_string()),
-                ..img
-            }).await.unwrap();
-            info!("{}", "添加图像成功".green());
-        } else {
-            info!("图像 {} 已存在", has.unwrap().title.clone().unwrap_or("无标题".to_string()));
+            let has = collection
+                .find_one(doc! { "startdate": &img.startdate, "mkt": market })
+                .await
+                .unwrap();
+            if has.is_none() {
+                collection
+                    .insert_one(create_bing_image(img, base_url, market))
+                    .await
+                    .unwrap();
+                info!("{}", "添加图像成功".green());
+            } else {
+                // 如果已存在的数据title和新数据的title不一致，替换数据
+                let local_img = has.unwrap();
+
+                if local_img.title != img.title {
+                    info!("本地图像标题: {}", local_img.title.clone().unwrap_or("无标题".to_string()));
+                    info!("新图像标题: {}", img.title.clone().unwrap_or("无标题".to_string()));
+                    collection
+                        .replace_one(
+                            doc! { "startdate": &img.startdate, "mkt": market },
+                            create_bing_image(img, base_url, market),
+                        )
+                        .await
+                        .unwrap();
+                    info!("{}", "替换图像成功".green());
+                } else {
+                    info!("{}", "图像已存在".yellow());
+                }
+            }
         }
     }
 }
@@ -63,8 +92,16 @@ pub async fn get_today_bing_images(collection: &State<Collection<BingImage>>) {
     let client = reqwest::Client::new();
 
     let locales = vec![
-        ("https://cn.bing.com/HPImageArchive.aspx", "zh-CN", "https://cn.bing.com"),
-        ("https://global.bing.com/HPImageArchive.aspx", "en-US", "https://www.bing.com"),
+        (
+            "https://cn.bing.com/HPImageArchive.aspx",
+            "zh-CN",
+            "https://cn.bing.com",
+        ),
+        (
+            "https://global.bing.com/HPImageArchive.aspx",
+            "en-US",
+            "https://www.bing.com",
+        ),
         // Add more locales here
     ];
 
@@ -72,11 +109,12 @@ pub async fn get_today_bing_images(collection: &State<Collection<BingImage>>) {
         fetch_and_store_image(
             &client,
             url,
-            &[("format", "js"), ("idx", "0"), ("n", "1"), ("mkt", market)],
+            &[("format", "js"), ("idx", "0"), ("n", "8"), ("mkt", market)],
             market,
             base_url,
             collection,
-        ).await;
+        )
+            .await;
     }
 }
 
@@ -98,7 +136,10 @@ fn build_filter(params: &FilterParams) -> Document {
 
 #[openapi]
 #[get("/bing_images?<params..>")]
-pub async fn get_bing_images(collection: &State<Collection<BingImage>>, params: FilterParams) -> Json<Vec<BingImage>> {
+pub async fn get_bing_images(
+    collection: &State<Collection<BingImage>>,
+    params: FilterParams,
+) -> Json<Vec<BingImage>> {
     let filter = build_filter(&params);
 
     let cursor = collection
