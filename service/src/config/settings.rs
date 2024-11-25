930307a666f5496198db25eb17269723f964ff99
vir <virs98@outlook.com>
2024-11-25 11:16:40 +0800

    feat(service): :fire: 删除无用代码

--- service/src/config/settings.rs
+++ service/src/config/settings.rs
@@ -1,7 +1,4 @@
-use std::env;
 use std::fs;
-
-use dotenv::dotenv;
 use serde::Deserialize;
 use toml;
 
@@ -18,25 +15,6 @@ pub struct MongoConfig {
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     pub mongo: MongoConfig,
-}
-
-pub fn get_settings() -> Settings {
-    dotenv().ok();
-
-    // 从环境变量构造 Settings
-    let mongo = MongoConfig {
-        host: env::var("MONGO_HOST").expect("MONGO_HOST must be set"),
-        port: env::var("MONGO_PORT")
-            .expect("MONGO_PORT must be set")
-            .parse()
-            .expect("MONGO_PORT must be a number"),
-        username: env::var("MONGO_USERNAME").expect("MONGO_USERNAME must be set"),
-        password: env::var("MONGO_PASSWORD").expect("MONGO_PASSWORD must be set"),
-        database: env::var("MONGO_DATABASE").expect("MONGO_DATABASE must be set"),
-        collection: env::var("MONGO_COLLECTION").expect("MONGO_DATABASE must be set"),
-    };
-
-    Settings { mongo }
 }
 
 pub fn get_settings_from_file(file_path: &str) -> Settings {
